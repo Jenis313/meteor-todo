99b4bdedfcef5e7cbedce540de9f4db0f9d861da
Jenis313 <jenishrai313@gmail.com>
2022-06-27 03:17:59 +1000

    last change

--- imports/api/tasksMethods.js
+++ imports/api/tasksMethods.js
@@ -25,6 +25,11 @@ Meteor.methods({
     if (!this.userId) {
       throw new Meteor.Error('Not authorized.');
     }
+    const task = TasksCollection.findOne({ _id: taskId, userId: this.userId });
+
+    if(!task){
+      throw new Meteor.Error('Access denied');
+    }
 
     TasksCollection.remove(taskId);
   },
@@ -37,6 +42,10 @@ Meteor.methods({
       throw new Meteor.Error('Not authorized.');
     }
 
+    const task = TasksCollection.findOne({ _id: taskId, userId: this.userId });
+    if(!task){
+      throw new Meteor.Error('Access denied.');
+    }
     TasksCollection.update(taskId, {
       $set: {
         isChecked
